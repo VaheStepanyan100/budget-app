10e3aac5348436a66c3a44580c020bd7b8cbb711
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-09-19 17:32:48 +0400

    Update config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/splash'
+  root 'static_pages#splash'
+  resources :categories do
+    resources :purchases, only: [:new, :show]
+  end
+  resources :purchases, except: [:new, :show, :index]
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
