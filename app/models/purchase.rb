e0eaa69987e99673428d1db1088b2b4869813573
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-09-19 17:03:25 +0400

    Add Purchase model with associations and validations

---
+++ app/models/purchase.rb
@@ -0,0 +1,8 @@
+class Purchase < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_and_belongs_to_many :categories
+
+  validates :name, presence: true
+  validates :amount, presence: true
+  validates :categories, presence: true
+end
