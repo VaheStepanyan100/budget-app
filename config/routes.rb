f5c9050fe3d123c632e8106efc0cab939c6288d9
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-09-19 17:37:31 +0400

    Add splash action to StaticPagesController

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   resources :purchases, except: [:new, :show, :index]
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
