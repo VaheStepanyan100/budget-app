b045b6621264bd44e118f694bd8b0a8a21e29277
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-09-20 16:00:15 +0400

    Add RSpec model tests for Purchase

---
+++ spec/models/purchase_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Purchase, type: :model do
+  let(:user) { User.create(email: 'user@example.com', password: 'password', name: 'John Doe') }
+  let(:category) { Category.create(name: 'Category', icon: 'icon', user: user) }
+  let(:purchase) { Purchase.new(name: 'Purchase Name', amount: 100, author: user) }
+
+  it 'is valid' do
+    purchase.categories << category
+    expect(purchase).to be_valid
+  end
+
+  it 'has a name' do
+    purchase.name = ''
+    expect(purchase).not_to be_valid
+  end
+
+  it 'has a valid amount' do
+    purchase.amount = nil
+    expect(purchase).not_to be_valid
+  end
+
+  it 'has an author associated' do
+    purchase.author = nil
+    expect(purchase).not_to be_valid
+  end
+
+  it 'associated author is valid' do
+    expect(purchase.author).to be_valid
+  end
+end
